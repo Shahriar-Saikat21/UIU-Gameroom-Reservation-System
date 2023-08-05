241417a635715aef300ae00539b0aca711ac437c
saikat <saikatbass@gmail.com>
2023-08-05 16:04:23 +0600

    Admin history,Search  Page route,alignment done

--- controller/adminController.js
+++ controller/adminController.js
@@ -5,9 +5,19 @@ adminController.adminHomePage = (req, res) => {
     res.render('adminHomePage');
 };
 
+//Admin See All Page
+adminController.adminSeeAllPage = (req, res) => {
+    res.render('adminHomePage3');
+};
+
 //Admin Create Attendance page
 adminController.adminCreateAttendancePage = (req, res) => {
     res.render('AdminAttendenceAccountCreate');
+};
+
+//Admin search Attendance page
+adminController.adminSearchAttendancePage = (req, res) => {
+    res.render('adminAttendenceAccountSearch');
 };
 
 //Admin Update Attendance page
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -6,8 +6,14 @@ const adminRoute = express.Router();
 //admin Home Page
 adminRoute.get('/adminHome',adminController.adminHomePage);
 
+//admin see all page
+adminRoute.get('/adminSeeAllHistory',adminController.adminSeeAllPage);
+
 //Admin Attendance create page
 adminRoute.get('/adminAttendantCreate',adminController.adminCreateAttendancePage);
+
+//Admin search Attendance page
+adminRoute.get('/adminAttendantSearch',adminController.adminSearchAttendancePage);
 
 //Admin Attendance Update page
 adminRoute.get('/adminAttendantUpdate',adminController.adminUpdateAttendancePage);
